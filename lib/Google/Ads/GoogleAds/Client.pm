b9e7196b6ce994e4db96adadb23b19602b861cc8
Laura Chevalier <laurachevalier4@users.noreply.github.com>
2024-01-23 11:10:07 -0500

    remove v13 (#119)

--- lib/Google/Ads/GoogleAds/Client.pm
+++ lib/Google/Ads/GoogleAds/Client.pm
@@ -21,7 +21,7 @@ package Google::Ads::GoogleAds::Client;
 use strict;
 use warnings;
 use version;
-our $VERSION = qv("19.0.0");
+our $VERSION = qv("20.0.0");
 
 use Google::Ads::GoogleAds::OAuth2ApplicationsHandler;
 use Google::Ads::GoogleAds::OAuth2ServiceAccountsHandler;
